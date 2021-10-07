a5e7df00dee2602d06a2316cc15bcf86e062212d
Rached Tlili <hichemhanafi012@gmail.com>
2021-10-07 11:33:52 +0100

    make it better

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ const card = require('./routes/card');
 const livreur = require('./routes/livreur')
 const category = require('./routes/catgory')
 const app = express();
+const path = require('path');
 
 app.use(express.json());
 
@@ -16,6 +17,14 @@ app.use('/category', category);
 app.use('/card', card);
 app.use('/user', user);
 app.use('/product', product);
+
+if(process.env.NODE_ENV==='production'){
+  app.use(express.static('client/build'));
+
+  app.get('*',(req,res)=>{
+    res.sendFile(path.resolve(process.cwd(),'client','build','index.html'));
+  })
+}
 
 const PORT = process.env.PORT || 8000;
 
